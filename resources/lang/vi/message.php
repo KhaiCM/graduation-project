d451926c7ea49a8ad0b332f0f8e823cb5764fe71
Dathoang997 <dathoang997@gmail.com>
2019-02-25 10:05:47 +0700

    template backend
    template backend fixed

---
+++ resources/lang/vi/message.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'Welcome' => 'Chào mừng',
+    'Admin' => 'Quản trị viên',
+    'New' => 'Tin Tức',
+    'Contact' => 'Liên hệ',
+    'About' => 'Thông tin',
+    'Account' => 'Tài khoản',
+    'Detail' => 'Chie Tiết',
+    'Logout' => 'Đăng xuất',
+    'Services' => 'Dịch vụ',
+    'Clients' => 'Khách hàng',
+    'Username' => 'Tên đăng nhập',
+    'Password' => 'Mật khẩu',
+    'Reset' => 'làm lại',
+    'Home' => 'Trang chủ',
+];
---
+++ resources/views/backend/layouts/master.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{ trans('message.Admin') }}</title>
+    <link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
+</head>
+<body>
+    <div class="topnav">
+        <a class="active" href="#home">{{ trans('message.Home') }}</a>
+        <a href="#news">{{ trans('message.New') }}</a>
+        <a href="#contact">{{ trans('message.Contact') }}</a>
+        <a href="#about">{{ trans('message.About') }}</a>
+        <div class="dropdown">
+            <button class="dropbtn">{{ trans('message.Account') }}</button>
+            <div class="dropdown-content">
+                <a href="#">{{ trans('message.Detail') }}</a>
+                <a href="#">{{ trans('message.Logout') }}</a>
+            </div>
+        </div>
+    </div> 
+   
+    <div class="sidenav">
+        <a href="#about">{{ trans('message.About') }}</a>
+        <a href="#services">{{ trans('message.Services') }}</a>
+        <a href="#clients">{{ trans('message.Clients') }}</a>
+        <a href="#contact">{{ trans('message.Contact') }}</a>
+    </div>
+
+    <div class="main">
+        @yield('content')
+    </div>
+
+</body>
+<footer>
+    <div class="footer">
+        <h2>{{ trans('message.Footer') }}</h2>
+    </div> 
+</footer>
+</html>
